24f3e2cf80d6280bd41f416b151eae9f6c607c45
Yusuf yıldız <ysfyldz0077@gmail.com>
2025-08-05 20:14:50 +0300

    refactor: remove unused database controller and routes

--- apps/database-api/src/controllers/databaseController.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { createSuccessResponse } from '../utils/response';
-import { logger } from '@tenanta/logging';
-
-export class DatabaseController {
-} 
--- apps/database-api/src/routes/databaseRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { DatabaseController } from '../controllers/databaseController';
-
-const router = Router();
-const databaseController = new DatabaseController();
-
-export default router; 
